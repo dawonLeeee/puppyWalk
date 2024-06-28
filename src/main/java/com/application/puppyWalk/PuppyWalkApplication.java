43d201daa1dc1f071e8e7d622506a04ae971e50b
dawon <ekdnjs210@gmail.com>
2024-06-28 19:59:04 +0900

    init project

---
+++ src/main/java/com/application/puppyWalk/PuppyWalkApplication.java
@@ -0,0 +1,13 @@
+package com.application.puppyWalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/puppyWalk/PuppyWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.puppyWalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
